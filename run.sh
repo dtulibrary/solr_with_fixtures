0737ff1f72d8f756df9ff422ddf6b18d7c2ec4f3
Jimmy Petersen <jipe@dtu.dk>
2016-02-03 10:53:03 +0100

    Add option to specify host port for mapping

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-docker run --rm=true -ti -P --name solr5 dtu/solr_with_fixtures:5.3
+port="-P"
+if [ $1 == '-p' ]; then
+  if [ -z $2 ]; then
+    echo "Missing port number"
+    exit 1
+  else
+    port="-p $2:8983"
+  fi
+fi
+exec docker run --rm=true -ti $port --name solr5 dtu/solr_with_fixtures:5.3
